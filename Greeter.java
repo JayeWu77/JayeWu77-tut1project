47a611dbe87feb0af8895ffaa078021f8c901b5c
Eva <2457216182@qq.com>
2021-09-12 21:04:32 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,10 +17,14 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
+    try{
     String language = args[0];
     String country = args[1];
     String name = args[2];
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
-  }
+  }catch (ArithmeticException e){
+      e.printStackTrace();
+      System.out.println("错误输入");
+    }
 }
