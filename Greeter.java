405848b5f75120607627408c75232a5eb8be91c0
Eva <2457216182@qq.com>
2021-09-12 21:10:00 +0800

    Added feature 2

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name;
+    return messages.getString("greeting") + ", " + "_"+name+"_";
   }
 
   public static void main(String[] args) {
